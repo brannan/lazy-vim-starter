13669289bf1b0397fd9ca8c4587dfaf95f738bc5
P. S. Brannan <psbrannan@pm.me>
2023-12-09 16:15:03 -0600

    gruvbox, restore examples

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.mapleader = ","
-vim.g.maplocalleader = "\\"
+-- vim.g.mapleader = ","
+-- vim.g.maplocalleader = "\\"
+
+-- vim.map("n", "<A-Up>", "<cmd>resize +2<cr>", { desc = "<A-Up> Increase windown height" })
+-- vim.map("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+-- vim.map("n", "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,30 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
   opts = {
     defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
       file_ignore_patterns = {
-        "node_modules",
-        "vendor",
-        "dist",
-        "build",
-        "target",
-        "cache",
-        "logs",
-        "tmp",
-        "deps",
-        "out",
-        "public",
-        "coverage",
+        "^node_modules/",
+        "^vendor/",
+        "^dist/",
+        "^build/",
+        "^target/",
+        "^cache/",
+        "^logs/",
+        "^tmp/",
+        "^out/",
+        "^deps/",
+        "^coverage/",
         "package%-lock.json",
         "%.lock",
       },
